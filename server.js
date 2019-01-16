97a566a5b44fef546ff04a8706fb61cb2cdf921d
Anthony Demott <java152001@gmail.com>
2019-01-16 01:35:52 -0700

    Updated API

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
   next();
 });
 
