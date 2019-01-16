d0b6aa3c283a41fdff731f555ea77470f174863c
Anthony Demott <java152001@gmail.com>
2019-01-16 00:41:46 -0700

    Updated search URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const api = {
   // Query NYT API 
   searchNYT: function(topic, start, end) {
-    const authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+    const authKey = "KRcvrq7fcxIWLhfoUmDzG6aKfgFq9AtW";
     const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
     authKey + "&q=" + topic + "&begin_date=" + start + "0101&end_date=" + end + "0101";
     return axios.get(queryURL);
